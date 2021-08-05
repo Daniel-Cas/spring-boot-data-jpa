f70299874ccfcea646cb980b44acf4a6b9238e37
Daniel-Cas <dmattcomics@gmail.com>
2021-08-05 15:00:12 -0500

    Primer commit, Clase 9 terminada, inicio de clase 10

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* Populate tables */
+
+INSERT INTO clientes ( id, nombre, apellido, email, create_at ) VALUES(1, 'Julieth','Segura','juls@gamil.com','2021-12-05');
+INSERT INTO clientes ( id, nombre, apellido, email, create_at ) VALUES(2, 'Samira','Castillo','sam@gmail.com','2020-05-05');
+INSERT INTO clientes ( id, nombre, apellido, email, create_at ) VALUES(3, 'Michelle','Castillo','chela@gmail.com','2020-04-04');
+
