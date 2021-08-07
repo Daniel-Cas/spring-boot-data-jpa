4f3581cb9364d1358cf5cf2bb5a5c6f37510eacc
Daniel-Cas <dmattcomics@gmail.com>
2021-08-07 12:12:01 -0500

    Seccion 8 Terminada, inicio Seccion 9 video 1 terminado, continuación video 2

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,13 +21,13 @@ import java.util.Map;
 public class ClienteController {
 
     @Autowired
-    private IClienteDao clienteDao;
+    private IClienteService clienteService;
 
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public String listar(Model model){
 
         model.addAttribute("titulo","Listado de clientes");
-        model.addAttribute("clientes", clienteDao.findAll() );
+        model.addAttribute("clientes", clienteService.findAll() );
 
         return "listar";
     }
@@ -48,7 +49,7 @@ public class ClienteController {
         Cliente cliente = null;
 
         if( id > 0 ){
-            cliente = clienteDao.findOne(id);
+            cliente = clienteService.findOne(id);
         } else {
             return "redirect:/listar";
         }
@@ -68,7 +69,7 @@ public class ClienteController {
             return "form";
         }
 
-        clienteDao.save(cliente);
+        clienteService.save(cliente);
         status.setComplete();
 
         return "redirect:listar";
@@ -78,7 +79,7 @@ public class ClienteController {
     public String eliminar( @PathVariable(value = "id") Long id ){
 
         if( id > 0 ){
-            clienteDao.delete(id);
+            clienteService.delete(id);
         }
 
         return "redirect:/listar";
