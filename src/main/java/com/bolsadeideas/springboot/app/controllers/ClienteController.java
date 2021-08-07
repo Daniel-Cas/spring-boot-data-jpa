7beec0a62507d2bb6a7fa6e5b543c8be4d6b5741
Daniel-Cas <dmattcomics@gmail.com>
2021-08-06 23:33:38 -0500

    Video 16 terminado, Inicio de video 17

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -74,5 +74,15 @@ public class ClienteController {
         return "redirect:listar";
     }
 
+    @RequestMapping(value = "/eliminar/{id}")
+    public String eliminar( @PathVariable(value = "id") Long id ){
+
+        if( id > 0 ){
+            clienteDao.delete(id);
+        }
+
+        return "redirect:/listar";
+    }
+
 
 }
--- src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -27,14 +27,21 @@ public class ClienteDaoImpl implements IClienteDao {
         if( cliente.getId() != null && cliente.getId() > 0){
              em.merge(cliente);
         } else {
-
              em.persist(cliente);
-
         }
     }
 
     @Override
+    @Transactional( readOnly = true)
     public Cliente findOne(Long id) {
         return em.find(Cliente.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+
+        em.remove( findOne(id) );
+
     }
 }
