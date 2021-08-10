d012c455e9185230912c0435ec42fad0f6739b6b
Daniel-Cas <dmattcomics@gmail.com>
2021-08-10 15:44:29 -0500

    Seccion 10 terminada

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -3,20 +3,35 @@ package com.bolsadeideas.springboot.app.controllers;
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
 import com.bolsadeideas.springboot.app.util.paginator.PageRender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @SessionAttributes("cliente")
@@ -24,6 +39,46 @@ public class ClienteController {
 
     @Autowired
     private IClienteService clienteService;
+
+    @Autowired
+    private IUploadFileService uploadFileService;
+
+
+
+
+
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String filename){
+
+
+        Resource recurso = null;
+        try {
+            recurso = uploadFileService.load(filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() +"\"" )
+                .body(recurso);
+    }
+
+
+
+    @GetMapping(value = "/ver/{id}")
+    public String ver( @PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash){
+        Cliente cliente = clienteService.findOne(id);
+
+        if( cliente == null ) {
+            flash.addFlashAttribute("error"," El cliente no existe en la base de datos");
+            return "redirect:/listar";
+        }
+
+        model.put("cliente", cliente);
+        model.put("titulo", "Detalle cliente: " + cliente.getNombre() );
+
+        return "ver";
+    }
 
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model){
@@ -74,11 +129,23 @@ public class ClienteController {
     }
 
     @RequestMapping( value = "/form", method = RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status){
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, @RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) throws IOException {
 
         if( result.hasErrors() ){
             model.addAttribute("titulo","Formulario de Cliente");
             return "form";
+        }
+        if( !foto.isEmpty() ){
+
+            if( cliente.getId() != null && cliente.getId() > 0 && cliente.getFoto() != null && cliente.getFoto().length() > 0 ){
+                uploadFileService.delete( cliente.getFoto() );
+            }
+
+            String uniqueFilename = uploadFileService.copy(foto);
+
+            flash.addFlashAttribute("info","Ha subido correctamente '" + uniqueFilename + "'");
+            cliente.setFoto( uniqueFilename );
+
         }
         String mensajeFlash = (cliente.getId() != null )? "Cliente editado con éxito!" : "Cliente creado con éxito";
 
@@ -93,8 +160,16 @@ public class ClienteController {
     public String eliminar( @PathVariable(value = "id") Long id,  RedirectAttributes flash ){
 
         if( id > 0 ){
+            Cliente cliente = clienteService.findOne(id);
+
             clienteService.delete(id);
             flash.addFlashAttribute("success", "Cliente eliminado con éxito");
+
+
+                 if( uploadFileService.delete(cliente.getFoto() ) ){
+                     flash.addAttribute("info", " Foto "+ cliente.getFoto() + " eliminada con exito!");
+                 }
+
 
         }
 
