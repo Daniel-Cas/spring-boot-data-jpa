207a128cda615c6ab879407b35be001e64352fca
Daniel-Cas <dmattcomics@gmail.com>
2021-08-13 23:38:17 -0500

    Error de JQuery solucionado, Terminar video 12

--- src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bolsadeideas.springboot.app.models.service;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.dao.IProductoDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,11 @@ public class ClienteServiceImpl implements IClienteService{
 
     @Autowired
     private IClienteDao clienteDao;
+
+    @Autowired
+    private IProductoDao productoDao;
+
+
 
 
     @Override
@@ -44,5 +51,11 @@ public class ClienteServiceImpl implements IClienteService{
     @Transactional
     public void delete(Long id) {
         clienteDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional( readOnly = true)
+    public List<Producto> findByNombre(String term) {
+        return productoDao.findByNombre(term);
     }
 }
