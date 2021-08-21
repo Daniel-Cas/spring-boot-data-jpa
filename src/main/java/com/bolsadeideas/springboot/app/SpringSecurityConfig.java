78ea728d4c77fbc6589a2e1049ad4293d77ffc2b
Daniel-Cas <dmattcomics@gmail.com>
2021-08-21 11:50:01 -0500

    Seccion 13 Spring Security, Video 9 terminado, Inicio del Video 10

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Seguridad HTTP -> Secure
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/css/**","/js/**","/images/**", "/listar").permitAll()
+                .antMatchers("/ver/**").hasAnyRole("USER")
+                .antMatchers("/uploads/**").hasAnyRole("USER")
+                .antMatchers("/form/**").hasAnyRole("ADMIN")
+                .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                .antMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                        .formLogin().loginPage("/login")
+                        .permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // Configuracion Global del login Es necesario en BCrypt que es lo que se usa actualmente
+    // TODO: revisar que metodos de encriptación son mejores para spring Security
+    @Autowired
+    public void configurerGlobal( AuthenticationManagerBuilder builder ) throws Exception{
+
+        PasswordEncoder enconder = passwordEncoder();
+        UserBuilder users = User.builder().passwordEncoder(  enconder::encode  );
+
+        builder.inMemoryAuthentication()
+                .withUser( users.username("admin").password("12345").roles("ADMIN","USER") )
+                .withUser( users.username("daniel").password("12345").roles("USER") );
+    }
+
+
+
+}
---
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(@RequestParam( value = "error", required = false) String error,
+            Model model, Principal principal, RedirectAttributes flash){
+
+        if( principal != null){
+
+            flash.addFlashAttribute("info","Ya ha iniciado sesión anteriormente");
+
+
+            return "redirect:/";
+        }
+
+        if(error != null){
+            model.addAttribute("error","Error en el login: Nombre o Contraseña incorrecto!");
+        }
+
+        return "login";
+    }
+
+}
