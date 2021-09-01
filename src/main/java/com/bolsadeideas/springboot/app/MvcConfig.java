085e8daf1d30b6fda4e29fc8d42ebc753c174034
Daniel-Cas <dmattcomics@gmail.com>
2021-08-31 23:32:44 -0500

    Plantilla 403 Creada, video 11 terminado

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -7,6 +7,7 @@ package com.bolsadeideas.springboot.app;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,6 +26,12 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/uploads/**")
 		.addResourceLocations(resourcePath);
 		*/
+
+
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry){
+		registry.addViewController("/error_403").setViewName("error_403");
 	}
 	
 	
