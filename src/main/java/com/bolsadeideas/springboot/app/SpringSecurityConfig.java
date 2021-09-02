708c98a980b91fd00e7d682fe3e41b1f1eece042
Daniel-Cas <dmattcomics@gmail.com>
2021-09-01 22:20:19 -0500

    Seccion 13 terminada

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.bolsadeideas.springboot.app;
 
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccesHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,22 +13,27 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginSuccesHandler successHandler;
 
     // Seguridad HTTP -> Secure
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/css/**","/js/**","/images/**", "/listar").permitAll()
-                .antMatchers("/ver/**").hasAnyRole("USER")
-                .antMatchers("/uploads/**").hasAnyRole("USER")
-                .antMatchers("/form/**").hasAnyRole("ADMIN")
-                .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-                .antMatchers("/factura/**").hasAnyRole("ADMIN")
+              /*  .antMatchers("/ver/**").hasAnyRole("USER") */
+              /*  .antMatchers("/uploads/**").hasAnyRole("USER")*/
+               /* .antMatchers("/form/**").hasAnyRole("ADMIN") */
+               /* .antMatchers("/eliminar/**").hasAnyRole("ADMIN")*/
+               /* .antMatchers("/factura/**").hasAnyRole("ADMIN")*/
                 .anyRequest().authenticated()
                 .and()
-                        .formLogin().loginPage("/login")
+                        .formLogin()
+                        .successHandler(successHandler)
+                        .loginPage("/login")
                         .permitAll()
                 .and()
                 .logout().permitAll()
--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -2,10 +2,14 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -13,6 +17,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -35,12 +46,15 @@ import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 @SessionAttributes("cliente")
 public class ClienteController {
 
+	protected final Log logger = LogFactory.getLog( this.getClass() );
+
 	@Autowired
 	private IClienteService clienteService;
 
 	@Autowired
 	private IUploadFileService uploadFileService;
 
+	@Secured("ROLE_USER")
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 
@@ -58,6 +72,7 @@ public class ClienteController {
 				.body(recurso);
 	}
 
+	@Secured("ROLE_USER")
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
@@ -72,8 +87,43 @@ public class ClienteController {
 		return "ver";
 	}
 
+
 	@RequestMapping(value = {"/listar","/"}, method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+						 Authentication authentication,
+						 HttpServletRequest request) {
+
+		if(authentication != null ){
+			logger.info("Hola usuario autenticado, tu username es: ".concat( authentication.getName() ));
+		}
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if( auth != null ){
+			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Hola usuario autenticado, tu username es: ".concat( authentication.getName() ) );
+		}
+
+		if( hasRole("ROLE_ADMIN")) {
+			logger.info("Hola ".concat( auth.getName() ).concat(" tienes acceso!."));
+		}else {
+			logger.info("Hola ".concat( auth.getName() ).concat(" no tienes acceso!."));
+		}
+
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request, "ROLE_");
+
+		if( securityContext.isUserInRole("ADMIN")){
+			logger.info(" Forma usando SecurityContextHolderAwareRequestWrapper:  " + "Hola ".concat( auth.getName() ).concat(" tienes acceso!."));
+		} else {
+			logger.info(" Forma usando SecurityContextHolderAwareRequestWrapper:  " + "Hola ".concat( auth.getName() ).concat(" NO tienes acceso!."));
+		}
+
+		if( request.isUserInRole("ROLE_ADMIN")){
+			logger.info(" Forma usando HttpServletRequest:  " + "Hola ".concat( auth.getName() ).concat(" tienes acceso!."));
+		} else {
+			logger.info(" Forma usando HttpServletRequest:  " + "Hola ".concat( auth.getName() ).concat(" NO tienes acceso!."));
+		}
+
+
 
 		Pageable pageRequest = PageRequest.of(page, 4);
 
@@ -86,6 +136,7 @@ public class ClienteController {
 		return "listar";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 
@@ -95,6 +146,7 @@ public class ClienteController {
 		return "form";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form/{id}")
 	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
@@ -115,6 +167,7 @@ public class ClienteController {
 		return "form";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
 			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
@@ -153,6 +206,7 @@ public class ClienteController {
 		return "redirect:listar";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 
@@ -168,5 +222,34 @@ public class ClienteController {
 
 		}
 		return "redirect:/listar";
+	}
+
+	private boolean hasRole(String role){
+
+		SecurityContext context = SecurityContextHolder.getContext();
+
+		if( context == null ){
+			return false;
+		}
+
+		Authentication auth = context.getAuthentication();
+
+		if(auth == null){
+			return false;
+		}
+
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+
+		return authorities.contains( new SimpleGrantedAuthority(role));
+		/*
+		for (GrantedAuthority authority : authorities){
+			if( role.equals( authority.getAuthority() ) ){
+				logger.info("Hola usuario ".concat( auth.getName() ).concat(" ru rol es: ").concat( authority.getAuthority() ));
+				return true;
+			}
+		}
+		return false;
+
+		 */
 	}
 }
